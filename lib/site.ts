3bc997622587c8e694b0cb19311ae1eab08f5003
Arthur-Henri Michalland <ah.michalland@gmail.com>
2026-05-26 13:24:58 +0200

    Update site.ts

--- lib/site.ts
+++ lib/site.ts
@@ -1,11 +1,11 @@
-export const SITE_NAME = "atu-web";
-export const SITE_URL =
-  process.env.NEXT_PUBLIC_SITE_URL ?? "https://atu-web.fr";
+export const SITE_NAME = "AtuWeb";
+export const SITE_URL = "https://atuweb.fr"
+  process.env.NEXT_PUBLIC_SITE_URL ?? "https://atuweb.fr";
 
 export const PERSON = {
   name: "Arthur-Henri Michalland",
   jobTitle: "Développeur web freelance",
-  email: "contact@atu-web.fr",
+  email: "contact@atuweb.fr",
   location: "Montpellier, France",
 } as const;
 
@@ -15,9 +15,7 @@ export const SAME_AS = [
   "https://scholar.google.com/citations?user=rQIx8ZoAAAAJ&hl=fr",
   "https://fr.linkedin.com/company/atuweb",
   "https://www.researchgate.net/profile/Arthur_Henri_Michalland",
-  "https://www.lirmm.fr/annuaire/?qq=Michalland+Arthur",
   "https://theses.hal.science/tel-02574831/",
-  "https://pmc.ncbi.nlm.nih.gov/articles/PMC8534254/",
 ] as const;
 
 export const LINKS = {
@@ -26,11 +24,7 @@ export const LINKS = {
   scholar:
     "https://scholar.google.com/citations?user=rQIx8ZoAAAAJ&hl=fr",
   linkedinCompany: "https://fr.linkedin.com/company/atuweb",
-  lirmm: "https://www.lirmm.fr/annuaire/?qq=Michalland+Arthur",
   thesis: "https://theses.hal.science/tel-02574831/",
-  publication:
-    "https://pmc.ncbi.nlm.nih.gov/articles/PMC8534254/",
-  webIris: "https://web-iris.lirmm.fr/",
   halSpaceSyntax: "https://hal.science/hal-03774890v1",
   ballab: "https://sites.google.com/view/annaborghilab/members",
 } as const;
